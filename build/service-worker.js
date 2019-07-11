f33d10658d39b03de85159c41c4aec51a8ce0c75
Dora Chan <dora.l.chan@gmail.com>
2019-07-10 23:05:53 -0700

    adding onboarding map

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f27110b5dd737ebc68eabbdd3ebd4f55.js"
+  "/precache-manifest.689d1aa06d9b2c8eda1b81dea7cfabd5.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/_work/project_onboarding.js
+++ src/components/_work/project_onboarding.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Modal from '../modal';
 
+import Mapping from '../../assets/project_onboarding_mapping.svg';
+
 class Onboarding extends Component {
   render() {
     return (
       <Modal
         title='Onboarding'
         handleClose={ this.props.closeModal }>
-        <Modal.Detail subtitle='The Problem'>
+        <Modal.Detail subtitle='The Problem' imgSrc={ Mapping }>
           TBD
         </Modal.Detail>
         <Modal.Detail subtitle='Research'>
