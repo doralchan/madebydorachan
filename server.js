debe391325258aa028b7acb0c7d79a0275003219
Dora Chan <dora.l.chan@gmail.com>
2019-08-05 21:10:07 -0700

    reverting

--- server.js
+++ server.js
@@ -15,18 +15,3 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-
-//force ssl
-var express = require('express');
-var http = require('http');
-var enforce = require('express-sslify');
-
-var app = express();
-
-// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
-// a load balancer (e.g. Heroku). See further comments below
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
