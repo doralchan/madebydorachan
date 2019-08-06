08592756bfbde8b69c7a05a82ff2ee87a8022f6b
Dora Chan <dora.l.chan@gmail.com>
2019-08-05 21:08:11 -0700

    added additional heroku line

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var app = express();
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku). See further comments below
-app.use(enforce.HTTPS());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
